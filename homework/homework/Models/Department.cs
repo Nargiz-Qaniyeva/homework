64c3b749b21166ce4f17b701339da4827f0abca6
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-04-05 00:59:41 +0400

    first commit

---
+++ homework/homework/Models/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace homework.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        private List<Employee> _employees;
+        public void AddEmployee()
+        {
+            Employee employee = new Employee(); 
+        }
+        public void Work(int id)
+        {
+            id++;
+            foreach (object item in _employees)
+            {
+                if (item == null) continue;
+                foreach (PropertyInfo property in item.GetType().GetProperties())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+
+
+    }   
+}
---
+++ homework/homework/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework.Models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public double  Salary { get; set; }
+        public void ShowInfo()
+        {
+
+        }
+    }
+}
